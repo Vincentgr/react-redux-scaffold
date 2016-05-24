e1e592147b6ce98fb3fb152c8095a114ca6cb07a
Greg Vincent <greg@gvincent.com>
2016-05-24 16:33:44 -0700

    Remove unused reference to redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@
 
 import { combineReducers } from 'redux';
 import exampleReducer from './reducers_example';
-import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   example: exampleReducer
